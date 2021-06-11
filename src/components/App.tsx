92bb463345516332b8a16f5b1bca18019ad2534d
Jaider Perez <jaider.perez@usantoto.edu.co>
2021-06-11 17:17:04 -0500

    feat: add components

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Content from "./content/Content";
+import Footer from "./footer/Footer";
+
+function App() {
+    return (
+        <React.Fragment>
+            <Content />
+            <Footer />
+        </React.Fragment>
+    );
+}
+
+export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="page-footer">
+            <footer className="footer-basic">
+                <div className="social">
+                    <a href="#"><i className="icon ion-social-instagram" /></a>
+                    <a href="https://github.com/jaidernperez?tab=repositories"><i className="icon ion-social-github" /></a>
+                    <a href="https://www.facebook.com/profile.php?id=100004864456093"><i className="icon ion-social-facebook" /></a>
+                </div>
+                <p className="copyright">Jaider Pérez © 2021</p>
+            </footer>
+        </footer>
+    );
+}
+
+export default Footer;
